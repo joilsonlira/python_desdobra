9f84352fc12c7a98e8894537e693fed37e187b6d
joilsonlira <joilson.lira@live.com>
2021-08-15 20:17:41 -0300

    iniciando desdobramento

--- construtor.py
+++ construtor.py
@@ -1,48 +1,61 @@
+from gerar import *
+
+
 class Construtor:
-
-    def __init__(self):
-        pass
-
-    def aposta():
-        pass
-
-    def geral():
-        pass
-
-    def conjunto():
-        pass
-
-    
-
-
-x=[
+    x=[
         [
             [1,2,3,4,5],
             [11,12,13,14,15],
-            [21,22,23,24,25]
+            [21,22,23,24,25],
+            [31,32,33,34,35],
+            [41,42,43,44,45]
+                
         ],
         [
             [6,7,8,9,10],
             [16,17,18,19,20],
-            [26,27,28,29,30]
-        ],
-        [
-            [31,32,33,34,35],
-            [41,42,43,44,45],
-            [51,52,53,54,55]
-        ],
-        [
+            [26,27,28,29,30],
             [36,37,38,39,40],
-            [46,47,48,49,50],
-            [56,57,58,59,60]
+            [46,47,48,49,50]
+        ],
+        [
+            [51,52,53,54,55],
+            [61,62,63,64,65],
+            [71,72,73,74,75],
+            [81,82,83,84,85],
+            [91,92,93,94,95]
+        ],
+        [
+            [56,57,58,59,60],
+            [66,67,68,69,70],
+            [76,77,78,79,80],
+            [86,87,88,89,90],
+            [96,97,98,99,100]
         ]
     ]
+    configuracoes_loteria = [1,6]
 
-for a in range(len(x)):
-    # print('a:',a)
-    for b in range(len(x[a])):
-        # print('b:',b)
-        for c in range(len(x[a][b])):
-            print(x[a][b][c])
+    id_aposta = Gerador.aleatorio(configuracoes_loteria)
+    print(id_aposta)
+
+    def __init__(self):
+        pass
+
+    
+    def aposta(id_aposta,x):
+        
+        aposta=[]
+        del aposta[:]
+
+        for a in range(len(id_aposta)):
+            
+            id_dezena = [id_aposta[a][0],id_aposta[a][1],id_aposta[a][2]]
+            aposta.append(x[id_dezena[0]][id_dezena[1]][id_dezena[2]])
+
+        return sorted(aposta)
+    
+    print(aposta(id_aposta,x))
+
+
 
 
--- gerar.py
+++ gerar.py
@@ -1,9 +1,56 @@
-class Gerar:
+from itertools import count
+import random as rand
+
+class Gerador:
 
     def __init__(self):
         pass
 
-    def aleatorio(args):
-        pass
+    def aleatorio(configuracoes_loteria):
 
+        aposta = []
+        del aposta[:]
+
+        id_dezena = []
+        del id_dezena[:]
+
+        id_aposta = []
+        del id_aposta[:]
+
+        for n in range(configuracoes_loteria[1]):
+
+            quadrante = rand.randint(0,3)
+            conjunto = rand.randint(0,4)
+            dezena = rand.randint(0,4)
+
+            if configuracoes_loteria[0] == 1:
+                if quadrante >= 2:
+                    conjunto = 0
+
+            id_dezena = [quadrante,conjunto,dezena]
+
+            if id_aposta.count(id_dezena):
+                id_dezena = []
+                del id_dezena[:]
+
+                quadrante = rand.randint(0,3)
+                conjunto = rand.randint(0,4)
+                dezena = rand.randint(0,4)
+                id_dezena = [quadrante,conjunto,dezena]
+                id_aposta.append(id_dezena)
+            else:
+                id_aposta.append(id_dezena)
     
+        return id_aposta
+
+
+    # print(aleatorio(x,configuracoes_loteria))
+
+
+
+
+
+
+
+
+
